d44171d4dace981540427fda7812cb79db85171f
ps9610 <ps961001@gmail.com>
2020-11-03 08:48:22 +0900

    신라호텔_슬라이드_버튼이벤트_완료

--- js/section1.js
+++ js/section1.js
@@ -1,6 +1,57 @@
 ;(function(window,document,$,undefinded){
     var cnt = 0;
 
+    function prevSlideFn(){
+        cnt--;
+        mainSlideFn();
+        fadeSlideFn();
+    };
+
+    function nextSlideFn(){
+        cnt++;
+        mainSlideFn();
+        fadeSlideFn();
+    };
+
+    function mainSlideFn(){
+        $(".slide-wrap").stop().animate({ left:-1920*cnt },0,function(){
+            if(cnt<0){
+                cnt=4;
+            }
+            if(cnt>4){
+                cnt=0;
+            }
+            // $(".slide-wrap").stop().animate({ left:-1920*cnt },0)
+        })
+    };
+
+    function fadeSlideFn(){
+        $(".slide-wrap").stop().animate({ opacity:0.7 },0,function(){
+            if(cnt<0){
+                cnt=4;
+            }
+            if(cnt>4){
+                cnt=0;
+            }
+            $(".slide-wrap").stop().animate({ opacity:1 }, 700)
+        })    
+    }   
+
+    $(".prev-btn").on({
+        click:function(){
+            if( !$(".slide-wrap").is(":animated") ){
+                prevSlideFn();
+            }
+        }
+    });
+
+    $(".next-btn").on({
+        click:function(){
+            if( !$(".slide-wrap").is(":animated") ){
+                nextSlideFn();
+            }
+        }
+    });
     
 
 })(window,document,jQuery);
