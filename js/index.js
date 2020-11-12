f596161f7663aca6d79b92250360a5c7c9b5f354
ps9610 <ps961001@gmail.com>
2020-11-12 14:59:28 +0900

    프로젝트_호텔신라_슬라이드_fadeIn_Out

--- js/index.js
+++ js/index.js
@@ -65,109 +65,101 @@
 
             var cnt = 0;
             var setId = 0;
-
-            setTimeout(initFn,100)
-
-            function nextSlideFn(){
-                cnt++;
-                if(cnt>4){cnt=0};
-                mainNextSlideFn();
-            };
-
+            
+            
             function prevSlideFn(){
-                cnt--;
-                if(cnt<0){cnt=4};
-                mainPrevSlideFn();
-            };            
-
-            function mainNextSlideFn(){
-                //다음 슬라이드가 현재 슬라이드(z-index:2)를 덮어 없앤다. fadeIn
-                $(".slide").css({ zIndex : 1 }).stop().animate({ opacity:1 },0)
-                $(".slide").eq( cnt==0? 4:cnt-1  /* 4 3 2 1 0 4 */ ).css({ zIndex : 2 })
-                $(".slide").eq( cnt             /* 0 4 3 2 1 0*/ ).css({ zIndex : 3 }).stop().animate({ opacity:1 },0).animate({ opacity:0 },800) 
-                // 현재 슬라이드 : cnt-1
-                // 원래 있던 슬라이드 opacity=1,가 천천히 사라짐 opacity:0; = 있었는데 없어지게함
-                // slide는 가장 나중에 배치한 4번부터 4 3 2 1 0의 순서로 zIndex 되어있음
-                // 슬라이드 0번이 있었는데 사라지고 4번이 나타남
-                // 슬라이드 4번이 있었는데 사라지고 3번이 나타남
-                // 슬라이드 3번이 있었는데 사라지고 2번이 나타남
-                // 슬라이드 2번이 있었는데 사라지고 1번이 나타남
-                // 슬라이드 1번이 있었는데 사라지고 0번이 나타남
-                pageBtnFn(cnt);
-            }
-            function mainPrevSlideFn(){
-                //현재 슬라이드가 걷어지고 이전 슬라이드가 나타난다. fadeOut
-                $(".slide").css({ zIndex : 1 }).stop().animate({ opacity:1 },0)
-                $(".slide").eq( cnt /* 0 4 3 2 1 0 */ ).css({ zIndex : 2 })
-                $(".slide").eq( cnt==4? 0:cnt+1 /* 4 3 2 1 0 4 */ ).css({ zIndex : 3 }).stop().animate({ opacity:0 },0).animate({ opacity:1 },800) 
-                //지금 현재 보이는 슬라이드는 cnt-1, cnt의 opacity가 1이 되면서 cnt번, 현재 슬라이드가 없어지고 opacity=0;
-                // slide는 가장 나중에 배치한 4번부터 4 3 2 1 0의 순서로 zIndex 되어있음
-                // css에서 z-index 순서 조절 -> 0 4 3 2 1로 포개져있음 (초기화상태)
-                // 슬라이드 1번을 보고싶으면(zIndex=2, zIndex=3으로 하면 opacity때문에 없어짐) 2번을 1번 위로 놓고 없어지게 해야 1번이 보임
-                // 슬라이드 2번을 보고싶으면(zIndex=2, zIndex=3으로 하면 opacity때문에 없어짐) 3번을 2번 위로 놓고 없어지게 해야 2번이 보임
-                // 슬라이드 3번을 보고싶으면(zIndex=2, zIndex=3으로 하면 opacity때문에 없어짐) 4번을 3번 위로 놓고 없어지게 해야 3번이 보임
-                // 슬라이드 4번을 보고싶으면(zIndex=2, zIndex=3으로 하면 opacity때문에 없어짐) 0번을 4번 위로 놓고 없어지게 해야 4번이 보임
-                // 슬라이드 0번을 보고싶으면(zIndex=2, zIndex=3으로 하면 opacity때문에 없어짐) 1번을 0번 위로 놓고 없어지게 해야 0번이 보임
-                pageBtnFn(cnt);
-            }
-            
-             $(".prev-btn").on({
-                click:function(){ 
-                    if( !$(".slide-wrap").is(":animated") ){ 
-                        prevSlideFn(); 
-                        clearInterval(setId);
-                    } 
+                    cnt--;
+                    if(cnt<0){cnt=3}
+                    if(cnt+1>3){cnt=0}
+                    if (cnt<0){cnt=3}
+                    //if(cnt-1<0){cnt-1==3}
+                    mainPrevSlideFn();
                 }
-            });
-            
+                
+                function nextSlideFn(){
+                    cnt++;
+                    if(cnt-1<0){cnt=3}
+                    if(cnt>3){cnt=0}
+                    mainNextSlideFn();
+                    }
+                //$(".slide").eq(3).stop().animate({opacity:1},0).css({zIndex:2})
+                //$(".slide").eq(0).stop().animate({opacity:1},2000).css({zIndex:3})
+    
+                //$(".slide").eq(0).stop().animate({opacity:1},0).css({zIndex:2})
+                //$(".slide").eq(1).stop().animate({opacity:1},2000).css({zIndex:3})
+                
+                //$(".slide").eq(1).stop().animate({opacity:1},0).css({zIndex:2})
+                //$(".slide").eq(2).stop().animate({opacity:1},2000).css({zIndex:3})
+                
+                //$(".slide").eq(2).stop().animate({opacity:1},0).css({zIndex:2})
+                //$(".slide").eq(3).stop().animate({opacity:1},2000).css({zIndex:3})
+                    
+                    // .slide-wrap .slide3 {z-index:2;opacity:0;}
+                    // .slide-wrap .slide0 {z-index:3;opacity:1;}
+                function mainNextSlideFn(){
+                    $(".slide").stop().animate({opacity:0},0).css({zIndex:1})
+                    $(".slide").eq(cnt).stop().animate({opacity:1},0).css({zIndex:2})    
+                    $(".slide").eq(cnt+1).stop().animate({opacity:1},500).css({zIndex:3})
+                    //console.log( cnt )
+                    pageFn(cnt);
+                    
+                    //$(".slide").eq(3).stop().animate({opacity:1},0).css({zIndex:2})
+                }
+                //mainNextSlideFn();
+
+                // setInterval(prevSlideFn,1000);
+                 setTimeout(prevSlideFn,1000);
+
+                function mainPrevSlideFn(){
+                    $(".slide").stop().animate({opacity:0},0).css({zIndex:1})
+                    $(".slide").eq(cnt).stop().animate({opacity:1},0).css({zIndex:2})                         
+                    $(".slide").eq(cnt).stop().animate({opacity:1},0).animate({opacity:0},500).css({zIndex:3})
+                    // 3 2 1 0 3 2 1
+                    console.log( cnt )
+                    //pageFn(cnt);
+                    }   
+                    // 3 6 5 4  3 6 5 4  3 ... 6=2 5=1 4=0 
+                    // 0 3 2 1  0 3 2 1  0 ...
+                    
+                    // 3 
+                    // 0 3
+
+
+
+                    //$(".slide").eq(3).stop().animate({opacity:1},0).css({zIndex:2})
+                    //$(".slide").eq(0).stop().animate({opacity:1},2000).css({zIndex:3})
+                    
+                    //$(".slide").eq(2).stop().animate({opacity:1},0).css({zIndex:2})
+                    //$(".slide").eq(3).stop().animate({opacity:1},2000).css({zIndex:3})
+                    
+                    //$(".slide").eq(1).stop().animate({opacity:1},0).css({zIndex:2})
+                    //$(".slide").eq(2).stop().animate({opacity:1},2000).css({zIndex:3})
+                    
+                    //$(".slide").eq(0).stop().animate({opacity:1},0).css({zIndex:2})
+                    //$(".slide").eq(1).stop().animate({opacity:1},2000).css({zIndex:3})
+
             $(".next-btn").on({
-                click:function(){
-                    if( !$(".slide-wrap").is(":animated") ){ 
-                        nextSlideFn(); 
-                        clearInterval(setId);
+                click : function(){
+                    if(!$(".slide").is(":animated")){
+                        nextSlideFn();
                     }
                 }
-            });
+            })
 
-            function initFn(){ 
-                setId = setInterval(nextSlideFn,5000) }
-            //initFn();
+            function pageFn(z){
+                z>3? z=0:z;
+                $(".page").removeClass("addPage")
+                $(".page").eq(z).addClass("addPage")
+                console.log(z);
+            }
 
             $(".page").each(function(index){
                 $(this).on({
                     click : function(){
-                        cnt=index;
-                        nextSlideFn();
-                        console.log(cnt)
-                        //clearInterval(setId);
+                        cnt = index;
+                        mainSlideFn();
                     }
                 })
-            });
-
-            function pageBtnFn(z){
-                z>4? z=0 : z;
-                $(".page").removeClass("addPage");
-                $(".page").eq(z).addClass("addPage");
-            }
-//.play-pause-btn {display:block;width:9px;height:12px;background:url(../img/main_prom_stop.png) no-repeat 50% 50%;}
-//.play-pause-btn.addPlay {background-image:url(../img/main_prom_play.png);}
-
-            var t = 0;
-
-            $(".play-pause-btn").on({
-                click : function(){
-                    if(t == 0){
-                        $(this).addClass("addPlay");
-                        t = 1;
-                        //setInterval 멈추는 clearInterval 설정 = 
-                    clearInterval(setId);
-                    }
-                    else if (t == 1){
-                        $(this).removeClass("addPlay");
-                        t = 0;
-                        initFn();
-                    }
-                }
             })
         },
 
@@ -206,12 +198,6 @@
 
             setInterval(rollingFn,3000);
             //setTimeout(rollingFn,1000);
-
-            //.notice-content > ul {display:none;position:absolute;top:0;left:0;line-height:24px;font-size:12px;text-align:center;color:#3e2b2c;}
-            //.notice-content > ul:first-child {display:block;top:-15px;line-height:24px;font-size:12px;text-align:center;color:#3e2b2c;}
-            //.notice-content > ul:nth-child(2) {display:block;top:0;line-height:24px;font-size:12px;text-align:center;color:#3e2b2c;}
-            //.notice-content > ul:nth-child(3) {display:block;top:0;line-height:24px;font-size:12px;text-align:center;color:#3e2b2c;}
-            //.notice-content > ul:nth-child(4) {display:block;top:0;line-height:24px;font-size:12px;text-align:center;color:#3e2b2c;}
             
             function rollingFn(){
                 cnt++;
@@ -225,7 +211,7 @@
                 $(".notice-content ul").eq(cnt==3?0:cnt+1).stop().animate({top:24},0).animate({top:0},1000);
                 // eq(0) (1) (2) (3) (0) cnt>3?0:cnt
                 // eq(1) (2) (3) (0) (1) cnt==3?0:cnt+1
-                console.log(cnt);   
+                //console.log(cnt);   
             }
             // rollingFn();
             // rollingFn();
